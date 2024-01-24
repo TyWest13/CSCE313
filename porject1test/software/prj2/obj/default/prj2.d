35cdd876a92841b8d6f6fe884644041e8e67299a
West <tydw@cocsce-l3d22-12.ds.sc.edu>
2024-01-24 09:21:36 -0500

    Got it running on board

---
+++ porject1test/software/prj2/obj/default/prj2.d
@@ -0,0 +1,18 @@
+obj/default/prj2.o: prj2.c ../prj2_bsp//HAL/inc/sys/alt_stdio.h \
+ ../prj2_bsp//HAL/inc/alt_types.h ../prj2_bsp/system.h \
+ ../prj2_bsp/linker.h ../prj2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../prj2_bsp//HAL/inc/io.h ../prj2_bsp//HAL/inc/alt_types.h
+
+../prj2_bsp//HAL/inc/sys/alt_stdio.h:
+
+../prj2_bsp//HAL/inc/alt_types.h:
+
+../prj2_bsp/system.h:
+
+../prj2_bsp/linker.h:
+
+../prj2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../prj2_bsp//HAL/inc/io.h:
+
+../prj2_bsp//HAL/inc/alt_types.h:
